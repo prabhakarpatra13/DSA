3afedda0f9be4ff8b513554c1631c3e23444d153
prabhakarpatra13 <prabhakarpatra13@gmail.com>
2021-11-23 21:47:15 +0530

    Multiplication Table
    Multiplication Table!
    If input is 3
    3 * 1 = 3 
     3 * 2 = 6 
     3 * 3 = 9 
     3 * 4 = 12 
     3 * 5 = 15 
     3 * 6 = 18 
     3 * 7 = 21 
     3 * 8 = 24 
     3 * 9 = 27 
     3 * 10 = 30

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+def main():
+    #Multiplication Table!
+
+    ip = int(input())
+    for i in range(1,11):
+        print(ip ,'*',i, '=',ip*i)
+    return 0
+
+if __name__ == '__main__':
+    main()
